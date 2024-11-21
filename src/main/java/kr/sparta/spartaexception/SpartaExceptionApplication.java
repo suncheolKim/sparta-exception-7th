b3714fae249f842bf27b929c225ce94202110bea
Suncheol Kim <tinymin@yahoo.co.kr>
2024-11-21 22:12:50 +0900

    first commit

---
+++ src/main/java/kr/sparta/spartaexception/SpartaExceptionApplication.java
@@ -0,0 +1,13 @@
+package kr.sparta.spartaexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaExceptionApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/sparta/spartaexception/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+    package kr.sparta.spartaexception.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+    @Configuration
+    public class SwaggerConfig {
+
+        @Bean
+        public OpenAPI customOpenAPI() {
+            SecurityScheme securityScheme = new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                    .in(SecurityScheme.In.HEADER).name("Authorization");
+            SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+            return new OpenAPI()
+                    .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                    .security(Collections.singletonList(securityRequirement))
+                    .info(getInfo());
+        }
+
+        private Info getInfo() {
+            return new Info()
+                    .title("API")
+                    .version("1.0")
+                    .description("API Document");
+        }
+    }
